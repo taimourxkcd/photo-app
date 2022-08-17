655c684ee4a28266b5e4f5e51ff4a6ad142ba926
taimourafzal <taimoor.afzal@arkhitech.com>
2022-08-17 16:49:35 +0500

    payment complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-   devise_for :users
+   devise_for :users, :controllers => { :registration => 'registration' }
   root 'welcome#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
